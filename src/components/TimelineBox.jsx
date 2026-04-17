fd0e55b6c3d06a0c63edb1d58a2a6a0892644135
Ahnaf-Afif <williamhens123@gmail.com>
2026-04-17 20:21:20 +0600

    timeline design done

--- src/components/TimelineBox.jsx
+++ src/components/TimelineBox.jsx
@@ -1,19 +1,42 @@
 "use client";
 
+import { Phone } from "lucide-react";
+import { MessageSquare } from "lucide-react";
+import { Video } from "lucide-react";
+
 import { useContext } from "react";
 import { Message_data } from "@/context/context";
 
 export default function TimelineBox() {
   const { message } = useContext(Message_data);
 
+  const today = new Date();
+
   return (
     <div>
-      <h1>Timeline</h1>
+      <h1 className="mt-5 text-3xl font-bold text-center mb-3">Timeline</h1>
       {message.map((item, index) => (
-        <div key={index}>
-          <p>Name: {item.name}</p>
-          <p>Type: {item.type}</p>
-          <p>Contact: {item.contact}</p>
+        <div
+          key={index}
+          className="card w-full max-w-11/12 mx-auto my-6 bg-base-100 card-xs shadow-sm"
+        >
+          <div className="card-body ">
+            <h2 className="flex flex-row gap-2 items-center">
+              {item.type === "Call" ? (
+                <Phone />
+              ) : item.type === "Text" ? (
+                <MessageSquare />
+              ) : item.type === "Video" ? (
+                <Video />
+              ) : null}
+              <span className="text-xl font-bold opacity-85">{item.type} </span>
+              <span className="text-sm opacity-80">with {item.name}</span>
+            </h2>
+            <div className="ml-9">
+              {today.getDate()}/ {today.getMonth() + 1}/ {today.getFullYear()}
+            </div>
+          </div>
+          <div></div>
         </div>
       ))}
     </div>
