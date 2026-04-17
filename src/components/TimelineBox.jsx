a3f46bc4271cfdf5ee58ef594d0de4358cc91b50
Ahnaf-Afif <williamhens123@gmail.com>
2026-04-17 20:43:55 +0600

    timeline filtering added

--- src/components/TimelineBox.jsx
+++ src/components/TimelineBox.jsx
@@ -4,41 +4,62 @@ import { Phone } from "lucide-react";
 import { MessageSquare } from "lucide-react";
 import { Video } from "lucide-react";
 
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Message_data } from "@/context/context";
 
 export default function TimelineBox() {
   const { message } = useContext(Message_data);
+  const [filter, setFilter] = useState("default");
 
   const today = new Date();
 
   return (
     <div>
       <h1 className="mt-5 text-3xl font-bold text-center mb-3">Timeline</h1>
-      {message.map((item, index) => (
-        <div
-          key={index}
-          className="card w-full max-w-11/12 mx-auto my-6 bg-base-100 card-xs shadow-sm"
+      <div className="join flex justify-center">
+        <div></div>
+        <select
+          className="select join-item"
+          onChange={(e) => setFilter(e.target.value)}
         >
-          <div className="card-body ">
-            <h2 className="flex flex-row gap-2 items-center">
-              {item.type === "Call" ? (
-                <Phone />
-              ) : item.type === "Text" ? (
-                <MessageSquare />
-              ) : item.type === "Video" ? (
-                <Video />
-              ) : null}
-              <span className="text-xl font-bold opacity-85">{item.type} </span>
-              <span className="text-sm opacity-80">with {item.name}</span>
-            </h2>
-            <div className="ml-9">
-              {today.getDate()}/ {today.getMonth() + 1}/ {today.getFullYear()}
+          <option value="default">All</option>
+          <option value="Text">Text</option>
+          <option value="Call">Call</option>
+          <option value="Video">Video</option>
+        </select>
+
+        <div className="indicator">
+          <button className="btn join-item">Search</button>
+        </div>
+      </div>
+
+      {[...message].reverse().map((item, index) =>
+        filter === "default" || filter === item.type ? (
+          <div
+            key={index}
+            className="card w-full max-w-11/12 mx-auto my-6 bg-base-100 card-xs shadow-sm"
+          >
+            <div className="card-body">
+              <h2 className="flex flex-row gap-2 items-center">
+                {item.type === "Call" ? (
+                  <Phone />
+                ) : item.type === "Text" ? (
+                  <MessageSquare />
+                ) : item.type === "Video" ? (
+                  <Video />
+                ) : null}
+                <span className="text-xl font-bold opacity-85">
+                  {item.type}
+                </span>
+                <span className="text-sm opacity-80">with {item.name}</span>
+              </h2>
+              <div className="ml-9">
+                {today.getDate()}/{today.getMonth() + 1}/{today.getFullYear()}
+              </div>
             </div>
           </div>
-          <div></div>
-        </div>
-      ))}
+        ) : null,
+      )}
     </div>
   );
 }
